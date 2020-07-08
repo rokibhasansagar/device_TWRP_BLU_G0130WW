bd4abe744a218444a859fb8f98ede4c4dd8ea904
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-07-08 16:27:22 +0600

    Prepare for PBRP

--- omni_G0130WW.mk
+++ omni_G0130WW.mk
@@ -2,11 +2,14 @@
 PRODUCT_RELEASE_NAME := G0130WW
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common_64.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/runtime_libart.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := BLU
